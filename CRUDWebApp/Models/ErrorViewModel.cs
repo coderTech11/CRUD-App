cbc8e4799bf0880dd1edcfcce1fe7f95324210d3
coderTech11 <codehub1@gmail.com>
2024-03-23 10:26:31 +0545

    changes

---
+++ CRUDWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRUDWebApp/context/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUDWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+namespace CRUDWebApp.context
+{
+    public class MyDbContext : DbContext
+    {
+
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) 
+        {
+
+        }
+    public DbSet<Product> Products { get; set; }
+    }
+}
